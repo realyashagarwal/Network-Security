32e236a27f1baac92a2669e44c460bbb741c31ac
Yash Agarwal <y2907ash@gmail.com>
2025-06-26 17:27:40 +0530

    Setup.py file is now ready

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements()->List[str]:
+    requirement_lst: List[str] = []
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found. Please ensure it exists in the current directory.")
+        
+    return requirement_lst
+
+setup(
+    name = "Network-Analysis",
+    version = "0.0.1",
+    author = "Yash Agarwal",
+    author_email = "y2907ash@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
